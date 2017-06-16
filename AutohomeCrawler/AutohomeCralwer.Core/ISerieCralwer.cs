18084eee06c9e9dbcd7c58b0f25be3ec0cd5761b
Bob Tian <chengxulvtu@126.com>
2017-06-16 16:47:35 +0800

    spider

--- AutohomeCrawler/AutohomeCralwer.Core/ISerieCralwer.cs
+++ AutohomeCrawler/AutohomeCralwer.Core/ISerieCralwer.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutohomeCralwer.Core.Models;
+
 namespace AutohomeCralwer.Core
 {
     public interface ISerieCralwer
     {
-
+        Task<IEnumerable<Serie>> GetSeriesAsync(GetBrandType type);
     }
 }
